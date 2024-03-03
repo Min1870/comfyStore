bd9f7e9de1b4f3fa26ce87c25056d02122100f17
minkhantzaw <minkhantzaw@g.com>
2024-03-03 20:55:42 +0630

    add about&hero page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,27 @@
-import React from "react";
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We Love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              Comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cumque
+        corrupti tempore ipsam, laboriosam perferendis similique exercitationem
+        earum voluptate illo labore id rerum minima nobis, commodi, fugiat
+        placeat. Nesciunt illum, eos quas saepe asperiores molestiae quam
+        perspiciatis quos modi illo.
+      </p>
+    </>
+  );
 };
 
 export default About;
---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const productionUrl = "https://strapi-store-server.onrender.com/api";
+
+const customFetch = axios.create({
+  baseURL: productionUrl,
+});
